edd36f68b4a1ca613f5d7a2806ad668b6cf9dd83
Ashwin 💖 <ashwin.j.2027@rkmshome.org>
2026-09-22 10:09:26 +0530

    Delete delete.php

--- delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "db.php";
-$id=intval($_GET["id"]??0); $lab_id=intval($_GET["lab_id"]??0);
-$stmt=mysqli_prepare($conn,"DELETE FROM stocks WHERE id=?"); mysqli_stmt_bind_param($stmt,"i",$id); mysqli_stmt_execute($stmt);
-header("Location: stocks.php?lab_id=$lab_id"); exit;
-?>
