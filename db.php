4da18eaf88ba699363d472d0ddcfe907ce7d1eaf
Ashwin 💖 <ashwin.j.2027@rkmshome.org>
2026-09-22 10:07:51 +0530

    Delete db.php

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "stock_management");
-if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
-}
-mysqli_set_charset($conn, "utf8mb4");
-?>
