8f8516f6ceb802b09a3563ee31bdfa72102ed2ca
Ashwin 💖 <ashwin.j.2027@rkmshome.org>
2026-09-22 10:08:32 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("form").forEach(form => {
-    form.addEventListener("submit", () => {
-      const btn = form.querySelector("button[type=submit], button[name]");
-      if (btn) btn.style.opacity = "0.75";
-    });
-  });
-});
